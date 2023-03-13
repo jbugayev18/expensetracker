670295dc9dffef1e7c563e189ffdbafad2a29dfd
Julia Bugayev <jbugayev18@gmail.com>
2023-03-12 18:47:05 -0700

    Added Expenses.js and ExpenseItem.js components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import React from "react";
 import './ExpenseItem.css'
-
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    const expenseDate= new Date(2023, 2, 9); 
-    const expenseTitle = 'Car Insurance'; 
-    const expenseAmount = 294.67; 
-
-
-
-
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
